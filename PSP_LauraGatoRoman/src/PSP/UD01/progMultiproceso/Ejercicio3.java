e91b9db1d1d5524b2b2ceacaff36abf1978af3c6
Laura <Laura@host.docker.internal>
2020-11-26 10:23:40 +0100

    Ejercicio3

--- PSP_LauraGatoRoman/src/PSP/UD01/progMultiproceso/Ejercicio3.java
+++ PSP_LauraGatoRoman/src/PSP/UD01/progMultiproceso/Ejercicio3.java
@@ -1,10 +1,43 @@
 package PSP.UD01.progMultiproceso;
 
+import java.io.BufferedReader;
+import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class Ejercicio3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Runtime rt = Runtime.getRuntime();
+		Process p = null;
+		
+		String comando = "CMD /C DIR";
+		
+		try {
+			p = rt.exec(comando);
+			
+			InputStream is = p.getInputStream();
+			BufferedReader br = new BufferedReader(new InputStreamReader(is));
+			
+			try (BufferedWriter filtroEscritura = new BufferedWriter(new FileWriter("ficheros.txt"))){
+				
+				String linea;
+				while ((linea = br.readLine()) != null) {
+					filtroEscritura.append(linea);
+					filtroEscritura.newLine();
+				}
+				
+				filtroEscritura.close();				
+			}
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
 }
