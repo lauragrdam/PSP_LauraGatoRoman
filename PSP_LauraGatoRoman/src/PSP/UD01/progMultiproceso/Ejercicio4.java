b1af507735abe183752e93f3254eddac8a65ba6c
Laura <Laura@host.docker.internal>
2020-11-30 15:48:53 +0100

    Ejercicio4

---
+++ PSP_LauraGatoRoman/src/PSP/UD01/progMultiproceso/Ejercicio4.java
@@ -0,0 +1,22 @@
+package PSP.UD01.progMultiproceso;
+
+public class Ejercicio4 {
+
+	public static void main(String[] args) {
+		
+		Runtime rt = Runtime.getRuntime();
+		Process p = null;
+		String comando = "CMD /C DIR";
+		int error = 0;
+		
+		try {
+			p = rt.exec(comando);
+			error = p.waitFor();
+		} catch (Exception e) {
+			e.getMessage();
+		}
+
+			System.out.println("El resultado de la ejecución es " + error);
+	}
+
+}
